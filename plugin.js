0d1593cfb1cb1088c23f6d9d36164648fd687f19
laredocctx <laredocctx@gmail.com>
2019-02-26 23:49:32 -0600

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -17,9 +17,9 @@ define(['playbackManager', 'pluginManager', 'browser', 'connectionManager', 'eve
 
         var self = this;
 
-        self.name = 'Default Skin';
+        self.name = 'unitymb.skin';
         self.type = 'skin';
-        self.id = 'defaultskin';
+        self.id = 'unitymb.skin';
 
         var dependencyPrefix = self.id;
         var settingsObjectName = dependencyPrefix + '/skinsettings';
